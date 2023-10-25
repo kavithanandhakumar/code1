c89e30d0a6cc6d3ce2fb9e0f7dbd95cf536ca289
nandhakumardhamodharan <nandhakumardhamodharan@localhost>
2023-10-25 10:36:26 -0700

    offer

--- src/hai/hai.java
+++ src/hai/hai.java
@@ -5,6 +5,8 @@ public class hai {
 	public static void main(String[] args) {
 		System.out.println("welcome");
 		System.out.println("congratz");
+		System.out.println("new offer");
+		System.out.println("100");
 
 	}
 
